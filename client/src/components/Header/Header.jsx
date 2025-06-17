931338cbcbab8ac357697fa6c45a17203eaf118f
pranjalr004 <pranjalr376@gmail.com>
2025-06-17 09:21:40 +0530

    Search Box created

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import {AppBar,Toolbar,styled,Box,Typography} from "@mui/material"
+import Search from "./Search"
 
 const StyledHeader=styled(AppBar)`
     background:#2874f0;
@@ -23,7 +24,7 @@ export default function Header() {
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
   return (
     <StyledHeader>
-        <Toolbar>
+        <Toolbar style={{minHeight:55}}>
             <Component>
                 <img src={logoURL} alt="logo" style={{width:75}}/>
                 <Box style={{display:"flex"}}>
@@ -35,6 +36,7 @@ export default function Header() {
                         <PlusImage src={subURL} alt="sub-logo"/>
                 </Box>
             </Component>
+            <Search/>
         </Toolbar>
     </StyledHeader>
   )
