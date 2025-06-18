1c2f0694a75d82c1beb570cda18291c337086614
pranjalr004 <pranjalr376@gmail.com>
2025-06-18 07:00:00 +0530

    Styling of carousel is completed

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Banner from './Banner'
+import { Box, styled } from '@mui/material'
+
+
+const Component=styled(Box)`
+padding:10px;
+background:#F2F2F2;`
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Component>
+      <Banner/>
+      </Component>
+    </>
+  
+  )
+}
