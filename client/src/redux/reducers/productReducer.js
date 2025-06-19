a83bced3d9a86578160424c3bd2013f473e6720e
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 22:03:14 +0530

    Redux is added

---
+++ client/src/redux/reducers/productReducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from '../constants/ProductConstant'
+
+export const getProductsReducer=(state={products:[]},action)=>{
+    switch(action.type){
+        case actionTypes.GET_PRODUCTS_SUCCESS:
+            return {products:action.payload}
+        case actionTypes.GET_PRODUCTS_FAIL:
+            return {error:action.payload}
+        default:
+            return state
+    }
+}
