7879c90e53f1e47d34206aaf0a72096302cae3d2
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 09:43:19 +0530

    dotenv file is created for username and password

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import Connection from './database/db.js';
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+import cors from 'cors'
 import DefaultData from './default.js';
+import Router from './routes/route.js'
+
 const app=express();
 dotenv.config()
+
+app.use(cors())
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/',Router)
+ 
 const PORT=8000;
 
 const USERNAME=process.env.DB_USERNAME;
 const PASSWORD=process.env.DB_PASSWORD;
 
 Connection(USERNAME,PASSWORD);
+
 app.listen(PORT,()=>console.log(`Server is running successfully on PORT ${PORT}`))
 
 DefaultData()
