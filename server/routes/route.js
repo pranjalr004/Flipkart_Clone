4791f42dc06c7329aef08f4a6f76710883dcf9d9
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 21:59:37 +0530

    Product route is added

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import {  userLogin, userSignup } from '../controller/userController.js'
-import { getProducts, getProductsById } from '../controller/productController.js'
+import { getProducts} from '../controller/productController.js'
 const router=express.Router()
 
 router.post('/signup',userSignup)
 router.post('/login',userLogin)
 
-
+router.get('/products',getProducts)
 
 
 
