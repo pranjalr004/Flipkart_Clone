4de9f3bf2802997286eb981cda4a31f197751ce1
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 09:39:10 +0530

    Node modules updated

---
+++ client/src/service/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const url='http://localhost:8000'
+
+
+export const authenticateSignup=async(data)=>{
+    try{
+        return await axios.post(`${url}/signup`,data)
+    }
+    catch(error){
+        console.log('Error while calling Signup API:',error)
+    }
+}
+
+
+
+export const authenticateLogIn=async(data)=>{
+    try{
+        return await axios.post(`${url}/login`,data)
+    }
+    catch(error){
+        console.log('Error while calling login API:',error)
+        return error.response
+    }
+}
