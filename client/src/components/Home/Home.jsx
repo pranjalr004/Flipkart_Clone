408586a8cbd0032dbc5ac6fc6fbb0468ce549818
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 22:00:34 +0530

    Dispatch Hook is used for fetching data

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Navbar from './Navbar'
 import Banner from './Banner'
 import { Box, styled } from '@mui/material'
-
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector} from 'react-redux'
+import {getProducts} from '../../redux/actions/productAction'
 const Component=styled(Box)`
 padding:10px;
 background:#F2F2F2;`
 
 export default function Home() {
+  const getProducts=useSelector(state=>state.getProducts)
+  const {products,error}=getProducts
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch])
   return (
     <>
       <Navbar/>
