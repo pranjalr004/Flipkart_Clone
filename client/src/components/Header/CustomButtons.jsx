fbfabe19b20f221983a8627a66c9d8badc403e86
pranjalr004 <pranjalr376@gmail.com>
2025-06-19 09:47:16 +0530

    Login Section is completed

--- client/src/components/Header/CustomButtons.jsx
+++ client/src/components/Header/CustomButtons.jsx
@@ -1,6 +1,9 @@
 import { Box,Button, styled, Typography } from '@mui/material'
 import {ShoppingCart} from "@mui/icons-material"
-import React from 'react'
+import React, { useState,useContext } from 'react'
+import LoginDialog from '../Login/LoginDialog'
+import  { LoginContext } from '../../context/ContextProvider'
+import Profile from './Profile'
 
 const Wrapper=styled(Box)`
 display:flex;
@@ -25,9 +28,17 @@ font-weight:600;
 height:32px;
 `
 export default function CustomButtons() {
+  const [open,SetOpen]=useState(false);
+  const {account,setAccount}=useContext(LoginContext)
+  const openDialog=()=>{
+    SetOpen(true)
+  }
   return (
     <Wrapper>
-      <LoginButton variant="contained">Login</LoginButton>
+      {
+        account ? <Profile account={account} setAccount={setAccount}/> :
+        <LoginButton variant='contained' onClick={()=>openDialog()}>Login</LoginButton>
+      }
       <Typography style={{marginTop:3,width:135}}>Become a Seller</Typography>
       <Typography style={{marginTop:3}}>More</Typography>
 
@@ -35,6 +46,7 @@ export default function CustomButtons() {
         <ShoppingCart/>
         <Typography>Cart</Typography>
       </Container>
+      <LoginDialog open={open} setOpen={SetOpen}/>
     </Wrapper>
   )
 }
