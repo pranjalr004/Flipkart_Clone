30553c8d8a3c178d444e3f21ce476d142319af66
pranjalr004 <pranjalr376@gmail.com>
2025-06-20 22:31:17 +0530

    Product Details are added

--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as actionTypes from '../constants/ProductConstant'
 
-
+const URL='http://localhost:8000'
 export const getProducts=()=>async(dispatch)=>{
     try{
         const {data}=await axios.get(`http://localhost:8000/products`)
@@ -9,5 +9,19 @@ export const getProducts=()=>async(dispatch)=>{
     }
     catch(error){
         dispatch({type:actionTypes.GET_PRODUCTS_FAIL,payload:error.message})
+    }
+}
+
+
+export const getProductDetails=(id)=>async(dispatch)=>{
+    try{
+        dispatch({type:actionTypes.GET_PRODUCT_DETAILS_REQUEST})
+
+        const {data}=await axios.get(`${URL}/product/${id}`)
+
+        dispatch({type:actionTypes.GET_PRODUCT_DETAILS_SUCCESS,payload:data})
+    }
+    catch(error){
+        dispatch({type:actionTypes.GET_PRODUCT_DETAILS_FAIL,payload:error.message})
     }
 }
