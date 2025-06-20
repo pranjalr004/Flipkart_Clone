e3c0c57a3c505585aed6018d2c2f032a86501114
pranjalr004 <pranjalr376@gmail.com>
2025-06-20 22:29:03 +0530

    BrowserRouter added for routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import ContextProvider from './context/ContextProvider';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import DetailView from './components/details/DetailView';
 
 function App() {
   return (
     <ContextProvider>
+      <BrowserRouter>
       <Header/>
       <Box style={{marginTop:54}}>
-      <Home/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/product/:id' element={<DetailView/>}/>
+        </Routes>
       </Box>
+      </BrowserRouter>
     </ContextProvider>
   );
 }
