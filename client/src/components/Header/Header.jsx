3a9f566dbe98102cbc681d63aa92ed528147eaac
pranjalr004 <pranjalr376@gmail.com>
2025-06-20 22:41:11 +0530

    Logo of Flipkart and SubLogo is added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import {AppBar,Toolbar,styled,Box,Typography} from "@mui/material"
 import Search from "./Search"
 import CustomButtons from "./CustomButtons"
+import { Link } from "react-router-dom"
 
 const StyledHeader=styled(AppBar)`
     background:#2874f0;
     height:55px;`
 
-const Component=styled(Box)`
+const Component=styled(Link)`
     margin-left:12%;
     line-height:0;
+    text-decoration:none;
+    color:inherit;
 `
 
 const SubHeading=styled(Typography)`
@@ -30,7 +33,7 @@ export default function Header() {
   return (
     <StyledHeader>
         <Toolbar style={{minHeight:55}}>
-            <Component>
+            <Component to='/'>
                 <img src={logoURL} alt="logo" style={{width:75}}/>
                 <Box style={{display:"flex"}}>
                     <SubHeading>Explore&nbsp; 
