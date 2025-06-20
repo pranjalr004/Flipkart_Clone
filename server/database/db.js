e8684bb8302224912f2381d357207ae85f7955a2
pranjalr004 <pranjalr376@gmail.com>
2025-06-20 22:36:30 +0530

    Database URL is updated and some fixes are changed

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 export const Connection=async(username,password)=>{
-    const URL=`mongodb://${username}:${password}@ac-v674pra-shard-00-00.s70zilm.mongodb.net:27017,ac-v674pra-shard-00-01.s70zilm.mongodb.net:27017,ac-v674pra-shard-00-02.s70zilm.mongodb.net:27017/?ssl=true&replicaSet=atlas-btvuv6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=E-Commerce-Web`;
+    // const URL=`mongodb://${username}:${password}@ac-v674pra-shard-00-00.s70zilm.mongodb.net:27017,ac-v674pra-shard-00-01.s70zilm.mongodb.net:27017,ac-v674pra-shard-00-02.s70zilm.mongodb.net:27017/?ssl=true&replicaSet=atlas-btvuv6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=E-Commerce-Web`;
+    const URL=`mongodb://user:g6kk6fhjjVYi88MK@ac-ujyznmb-shard-00-00.t0l6vxg.mongodb.net:27017,ac-ujyznmb-shard-00-01.t0l6vxg.mongodb.net:27017,ac-ujyznmb-shard-00-02.t0l6vxg.mongodb.net:27017/ECommerce?ssl=true&replicaSet=atlas-ugm2vy-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ecommerce-web`
     try{
        await mongoose.connect(URL)
        console.log("Database Connected Successfully")
